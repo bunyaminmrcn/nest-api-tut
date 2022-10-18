081d787e0096a5fe7b61312a8bf9d4f5b2a58d4e
mrcnbunyamin <mrcnbunyamin@gmail.com>
2022-10-18 13:51:01 +0300

    catch double email(email field unique) record

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 @Injectable()
 export class AuthService {
 
@@ -11,15 +12,26 @@ export class AuthService {
     async signup(dto : AuthDto) {
         
         let hash = await argon.hash(dto.password)
-        const user = await this.prismaService.user.create({
-            data: {
-                email: dto.email,
-                hash
-            }
-        })
 
-        delete user.hash
-        return user;
+        try {
+            const user = await this.prismaService.user.create({
+                data: {
+                    email: dto.email,
+                    hash
+                }
+            })
+    
+            delete user.hash
+            return user;
+        } catch (error) {
+            if(error instanceof PrismaClientKnownRequestError) {
+                if(error.code == 'P2002') {
+                    throw new ForbiddenException('Credentials taken')
+                }
+                throw error;
+            }
+        }
+        
     }
 
     signin() {
