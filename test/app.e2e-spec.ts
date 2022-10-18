021f39be66ffbf86dbca9903073f20dd2d5e8a3a
mrcnbunyamin <mrcnbunyamin@gmail.com>
2022-10-18 16:36:13 +0300

    'pre' usage

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,16 +1,29 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 
 import { AppModule } from '../src/app.module';
 
 describe('App (e2e)', () => {
-  
+  let app: INestApplication;
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist: true
+    }));
+
+    await app.init()
   });
+
+  /*
+  afterAll(() => {
+    app.close()
+  })
+  */
 
   it.todo('should pass')
 });
