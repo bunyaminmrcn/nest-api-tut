d006c53913929a031d935ffb4c9447152d1c9aee
mrcnbunyamin <mrcnbunyamin@gmail.com>
2022-10-19 09:09:37 +0300

    All Test and Some Logic

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,24 +1,30 @@
-import { Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { User } from '@prisma/client';
-import { Request } from 'express';
-import { GetUser } from '../auth/decorators';
-import { JwtGuard } from '../auth/guard';
-
-@UseGuards(JwtGuard)
-@Controller('users')
-export class UserController {
-
-    
+import {
+    Body,
+    Controller,
+    Get,
+    Patch,
+    UseGuards,
+  } from '@nestjs/common';
+  import { User } from '@prisma/client';
+  import { GetUser } from '../auth/decorator';
+  import { JwtGuard } from '../auth/guard';
+  import { EditUserDto } from './dto';
+  import { UserService } from './user.service';
+  
+  @UseGuards(JwtGuard)
+  @Controller('users')
+  export class UserController {
+    constructor(private userService: UserService) {}
     @Get('me')
-    getMe(@GetUser('id') userId: number, @GetUser('email') email: number) {
-
-        console.log(email)
-        return userId
+    getMe(@GetUser() user: User) {
+      return user;
     }
-
+  
     @Patch()
-    editUser() {
-
+    editUser(
+      @GetUser('id') userId: number,
+      @Body() dto: EditUserDto,
+    ) {
+      return this.userService.editUser(userId, dto);
     }
-}
+  }
